d1c20adac19d92b9cf73e19cff4340a46350ca66
thetopham <124335168+thetopham@users.noreply.github.com>
2024-01-24 01:27:37 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,16 +34,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://thetopham.com"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            thetopham.com
-          </Link> to start my developer portfolio.
-        </h2>
+        
       </div>
     </div>
   );

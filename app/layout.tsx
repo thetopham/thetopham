b506a5cdb3fb993dfe81895ac144b4e6e9264ff1
thetopham <124335168+thetopham@users.noreply.github.com>
2024-01-23 22:59:55 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "thetopham.com",
+    template: "%s | thetopham.com",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Future Computer Science Graduate",
   openGraph: {
-    title: "chronark.com",
+    title: "thetopham.com",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Future Computer Science Graduate",
+    url: "https://thetopham.com",
+    siteName: "thetopham.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://thetopham.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "thetopham",
     card: "summary_large_image",
   },
   icons: {

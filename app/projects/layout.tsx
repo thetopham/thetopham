f5bc95310997f1963df538c6943f9d25a1a33876
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-08 21:09:44 -0700

    Update layout.tsx

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,4 +1,5 @@
 // projects/layout.tsx
+import Particles from "../components/particles";
 import MatrixRainingCode from "../components/MatrixRainingCode";
 
 export default function ProjectsLayout({
@@ -7,7 +8,7 @@ export default function ProjectsLayout({
   return (
     <div className="relative min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black overflow-x-hidden">
       <MatrixRainingCode /> {/* Matrix rain effect */}
-     
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
       {children}
     </div>
   );

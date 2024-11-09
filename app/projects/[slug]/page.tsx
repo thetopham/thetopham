9cd4b23eadb88b54d2709de59579328977dffbd0
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-09 13:22:42 -0700

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function PostPage({ params }: Props) {
   const views =
     (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
   return (
-    <div className="bg-zinc-50 min-h-screen">
+    <div className="bg-transparent min-h-screen">
       <Header project={project} views={views} />
       <ReportView slug={project.slug} />
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">

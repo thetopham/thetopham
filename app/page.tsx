6c8639e757f350d43665743fa9d0f6d29004cd5b
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-09 11:19:49 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Particles from "./components/particles";
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
+  { name: "AI", href: "/ai" },
 ];
 
 export default function Home() {

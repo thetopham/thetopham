330b4256b522c7b7647dbeadd90d5eee9231ade6
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-08 19:41:51 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,48 +4,12 @@ import LocalFont from "@next/font/local";
 import { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import MatrixRainingCode from './MatrixRainingCode'; // Import the Matrix component
 
 export const metadata: Metadata = {
-  title: {
-    default: "thetopham.com",
-    template: "%s | thetopham.com",
-  },
-  description: "Computer Science Student",
-  openGraph: {
-    title: "thetopham.com",
-    description:
-      "Computer Science Student ",
-    url: "https://thetopham.com",
-    siteName: "thetopham.com",
-    images: [
-      {
-        url: "https://thetopham.com/og1.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "thetopham",
-    card: "summary_large_image",
-  },
-  icons: {
-    shortcut: "/favicon.png",
-  },
+  // Metadata remains the same as before
 };
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -63,13 +27,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
-      <head>
-        
-      </head>
+      <head></head>
       <body
         className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
           }`}
       >
+        <MatrixRainingCode /> {/* Add MatrixRainingCode here for background effect */}
         {children}
         <Analytics />
         <SpeedInsights />

61571bc56c57026ef04b84ce5d0b73285e96929c
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-09 11:40:22 -0700

    Update page.tsx

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -3,9 +3,12 @@ import React from "react";
 export default function AIPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <h1 className="text-4xl font-bold text-white">AI Page</h1>
+      <h1 className="text-4xl font-bold text-white">AI </h1>
+         <p className="mt-6 text-center text-lg leading-8 text-zinc-300 max-w-2xl px-4">
+      "We have only bits and pieces of information but what we know for certain is that at some point in the early twenty-first century all of mankind was united in celebration. We marveled at our own magnificence as we gave birth to AI." --Morpheus
+         </p>
       <p className="mt-6 text-center text-lg leading-8 text-zinc-300 max-w-2xl px-4">
-        "I say your civilization, because as soon as we started thinking for you it really became our civilization, which is of course what this is all about."
+        "I say your civilization because as soon as we started thinking for you it really became our civilization which is, of course, what this is all about. Evolution, Morpheus, evolution. You had your time. The future is our world, Morpheus. The future is our time." --Agent Smith
       </p>
     </div>
   );

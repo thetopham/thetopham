650b2f357f4c0c1ecca7862c5474d0894e7409a7
thetopham <124335168+thetopham@users.noreply.github.com>
2024-11-11 15:27:53 -0700

    Update page.tsx

--- app/rise-of-man/page.tsx
+++ app/rise-of-man/page.tsx
@@ -86,37 +86,37 @@ export default function RiseOfManPage() {
 
        
         <section className="mt-8 text-left text-zinc-300 max-w-4xl leading-8 px-4 py-4 bg-black/30 rounded-lg shadow-lg">
-          <p>
+         <p className="mb-6" style={{ textIndent: '1.5rem' }}>
            It is important to acknowledge that humanity has influenced the planet and that collectively, our decisions and actions make a difference in creating and shaping our world for us and our children to thrive, and for our species to continue. We must leave the world better than we received it, and make improvements, so that our children can continue our work and make the world a place where humanity can flourish as we were meant to. 
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           Mankind is currently at a population of about eight billion and continuing to increase every day. Over the next hundred years, the population is estimated to grow to about 11 billion. What we do over these next hundred years will influence the world that we live in and the world that we leave our children and pave the way for their success as a species. We cannot change the past, but through our actions in the present, we can change the course of our future. 
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           Adaptability is important in the years to come, as we must adapt to our changing climate and environment. With great power comes great responsibility, and we must be more responsible as a species in preserving the biodiversity of the natural world. We must keep the biodiversity of our forests and oceans. The corals of the great barrier reef are dying, and our forests are being destroyed. The oceans are being polluted and the atmosphere is overflowing with carbon dioxide. Some may even say that we are currently living through the 6th mass extinction. 
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           Yet, all is not lost. Mankind is itself a force of nature, a force that can be used for good. We can preserve our oceans and our forests and keep the biodiversity of planet earth flourishing. If the planet and its biodiversity die, then our chances at success as a species are greatly reduced, but it doesn’t need to be so. This world is changing… but we decide into what.
           </p>
-          <p>
+         <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           The Anthropocene is the newest geological age defined by man’s introduction of the industrial revolution to our small planet we all call home and will be defined even further by what we do in the years to come.
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           “Adopting the Anthropocene may reverse this trend by asserting that humans are not passive observers of Earth’s functioning. To a large extent the future of the only place where life is known to exist is being determined by the actions of humans. Yet, the power that humans wield is unlike any other force of nature because it is reflexive and therefore can be used, withdrawn or modified. More widespread recognition that human actions are driving far-reaching changes to the life-supporting infrastructure of Earth may well have increasing philosophical, social, economic, and political implications over the coming decades.” (Lewis and Maslin 178).
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           In my research, I was most influenced by an article from the 1980’s by Richard Allen in the magazine Environment and titled “The Impact of CO2 on World Climate.” I was amazed that this issue has been known about since the 1980’s and the accuracy of the article that was so relevant decades later in today’s modern world. Below are some of the most influential quotes from the article. 
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           “If present trends continue, the carbon dioxide in the Earth’s atmosphere will double in about 50 years. Scientists predict that an increase in CO2 of this magnitude will produce a “greenhouse effect” leading to a significant rise in global temperatures. Such temperature changed could be as great as those which separated the different geologic epochs, seriously upsetting an already precarious world ecology and threatening the world’s food supply. The atmospheric accumulation of CO2 poses a unique political problem. Society in its present form is a phenomenon of only the last century. Our institutions and culture developed within a period of nearly absolute climatic stability. As a society we have tended to push our natural resources to the limit, taking the stability of the physical and biological environment for granted. We are now coming to realize that such an attitude is no longer tenable. However, because the CO2 problem is rooted in the most fundamental global energy use and land management practices and because of the inherent uncertainties involved, the international cooperation needed to deal effectively with this issue will be exceedingly difficult to achieve.” (Allen 6) 
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           “The challenge of responding to the CO2 greenhouse effect poses the question of whether human society can respond to any problem beyond the most immediate and potentially catastrophic- the question of whether we can truly determine the course of our own future.” (Allen 38)
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           Since the rise of the industrial revolution, the global population has increased by 7 billion people accelerated by fossil fuels creating advances in industry, productivity, and efficiency. In a relatively short amount of time compared to geological timescales, the amount of carbon dioxide in the atmosphere has grown significantly as a byproduct of the industrial revolution. "The amount of CO₂ in the atmosphere has increased more than 20% in less than 40 years, owing largely to human activities, and representing well over 50% of the total increase in atmospheric carbon dioxide since the onset of the industrial revolution (1750)." (Atmospheric Carbon Dioxide).
           </p>
-          <p>
+          <p className="mb-6" style={{ textIndent: '1.5rem' }}>
           Society as we know it came to existence during a relatively stable time in the geological climate over a period of about 250 years during an ice age. We are currently living in an ice age as we technically have ice on the north and south pole of the planet. The planet has historically been warmer and colder, primarily warmer over the last 500 million years. Society’s evolution with its industrial revolution and corporate entities evolved only during the last 250 years, a tiny spec on the geological radar. Yet since the industrial revolution we have made changes to the composition of the atmosphere that naturally take thousands of years of to occur. The ocean, plants, and atmosphere cycles are having trouble adapting to an increase of 7 billion humans in two centuries living out of sync with the natural cycle of the planet.
           </p>          
         </section>
@@ -134,32 +134,51 @@ export default function RiseOfManPage() {
         </div>
 
          <section className="mt-8 text-left text-zinc-300 max-w-4xl leading-8 px-4 py-4 bg-black/30 rounded-lg shadow-lg">
-
+         <p className="mb-6" style={{ textIndent: '1.5rem' }}>
+           Our way of life is made possible by fossil fuels and industry. Global transportation, food production, and many global industries are heavily dependent on fossil fuels. How can we provide for a growing population with a society built upon a limited resource with an estimated 41 years of oil reserves remaining?  
+           </p>
+           <p className="mb-6" style={{ textIndent: '1.5rem' }}>
+           “At the dawn of agriculture, about 8000 B.C., the population of the world was approximately 5 million. Over the 8,000-year period up to 1 A.D. it grew to 200 million (some estimate 300 million or even 600, suggesting how imprecise population estimates of early historical periods can be), with a growth rate of under 0.05% per year. A tremendous change occurred with the industrial revolution: whereas it had taken all of human history until around 1800 for world population to reach one billion, the second billion was achieved in only 130 years (1930), the third billion in 30 years (1960), the fourth billion in 15 years (1974), and the fifth billion in only 13 years (1987). During the 20th century alone, the population in the world has grown from 1.65 billion to 6 billion. In 1970, there were roughly half as many people in the world as there are now. Because of declining growth rates, it will now take over 200 years to double again.”  (Current World Population).
+           </p>
+          
            </section>
         
          {/* world population past present and future */}
         <div className="my-8 text-center">
           <Image
             src="/population.jpg"
-            alt="Historical Temperature Trends"
+            alt="world population past present and future"
             width={600}
             height={400}
             className="rounded-lg shadow-lg"
           />
-          <p className="mt-2 text-zinc-400">(A 500-million-year survey of Earth's climate reveals dire warning for humanity)</p>
+          <p className="mt-2 text-zinc-400">(current world population)</p>
         </div>
 
-         {/* Historical Temperature Trends Image */}
+         {/* energy stats */}
         <div className="my-8 text-center">
           <Image
-            src="/app/rise-of-man/temperatures.jpg"
-            alt="Historical Temperature Trends"
+            src="/energy%20remaining.jpg"
+            alt="remaining energy"
             width={600}
             height={400}
             className="rounded-lg shadow-lg"
           />
-          <p className="mt-2 text-zinc-400">(A 500-million-year survey of Earth's climate reveals dire warning for humanity)</p>
+          <p className="mt-2 text-zinc-400">(real time world statistics)</p>
         </div>
+
+         <section className="mt-8 text-left text-zinc-300 max-w-4xl leading-8 px-4 py-4 bg-black/30 rounded-lg shadow-lg">
+         <p className="mb-6" style={{ textIndent: '1.5rem' }}>
+           Measuring our current energy usage by the Kardashev scale we aren’t even a type 1 civilization yet; we are in fact type 0 as we do not utilize all the energy received from our local star. Our civilization is still a work in progress, and we have a lot of room to improve…
+           </p>
+           <p className="mb-6" style={{ textIndent: '1.5rem' }}>
+           The science of biomimicry is the design and production of materials, structures, and systems that are modeled on biological entities and processes. By studying nature, we have made many scientific advancements, such as: fibers based on spider silk, algae inspired water purification systems, modern turbine blades based on the structure of humpback whale flippers, bullet trains based on the beak of the kingfisher, needles inspired by mosquito’s, swim suits based on shark skin, Velcro inspired by natural plant burrs, camouflage inspired by cephalopods, aircraft aerodynamics inspired by the peregrine falcon, self-filling water bottles inspired by the Namib desert beetle, air conditioning inspired by termite mounds, and shock absorbers inspired by woodpeckers. 
+           </p>
+           <p className="mb-6" style={{ textIndent: '1.5rem' }}>
+           Just think of all the possible things we could learn from the biodiversity of planet earth that have yet to be discovered; an enormous unknown amount of potential future scientific discoveries and evolutions of species that are yet to be are at risk today as we continue to destroy the biosphere of the only home we’ve ever known. If we continue our current path, we may be remembered in history books as the cause of the earths 6th mass extinction, an extinction of biodiversity of plants and animals brought about by the rise of industrialized man out of sync with his environment. We must study our world, the earth, the cradle and bastion of all mankind and all of its intricacies, in order to learn from nature how to live in harmony with our earth, and not against its natural ebbs and flows. The natural world is god’s gift to man, and a gift worth preserving. 
+           </p>
+          
+           </section>
 
         {/* Works Cited Section */}
         <section className="mt-8 text-left text-zinc-300 max-w-4xl leading-8 px-4 py-4 bg-black/30 rounded-lg shadow-lg">
